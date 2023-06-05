2cd8a7c2a7af27963db3dd9661ac5339e4145354
gregor <gregor@MacBook-Pro-von-crecker.local>
2023-06-05 14:45:40 +0100

    back

--- fragment.glsl
+++ fragment.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
+
 varying vec4 vColor;
+
 void main() {
   gl_FragColor = vColor;
 }
--- vertex.glsl
+++ vertex.glsl
@@ -1,7 +1,10 @@
 attribute vec4 aPosition;
 attribute vec4 aColor;
+
 uniform mat4 uMVPMatrix;
+
 varying vec4 vColor;
+
 void main() {
   gl_Position = uMVPMatrix * aPosition;
   vColor = aColor;
