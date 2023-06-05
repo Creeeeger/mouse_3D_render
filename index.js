6648d0871ec70620c1537625e9b4edfa00e39c52
gregor <gregor@MacBook-Pro-von-crecker.local>
2023-06-05 14:55:34 +0100

    back1

--- index.js
+++ index.js
@@ -1,208 +1,175 @@
 (function(global) {
-
-  /*
-  * Constants and Main
-  * www.programmingtil.com
-  * www.codenameparkerllc.com
-  */
-  var state = {
-    gl: null,
-    program: null,
-    ui: {
-      dragging: false,
-      mouse: {
-        lastX: -1,
-        lastY: -1,
-      },
-      pressedKeys: {},
-    },
-    animation: {},
-    app: {
-      angle: {
-        x: 0,
-        y: 0,
-      },
-      eye: {
-        x:2.,
-        y:2.,
-        z:7.,
-      },
-    },
+  var signals = {
+    VERSION: '0.6.3'
   };
 
-  // Cube verts
-  var DEFAULT_VERT = [
-     1, 1, 1, 1, 1,1,1,1,
-    -1, 1, 1, 1, 1,0,0,1,
-    -1,-1, 1, 1, 0,1,0,1,
-     1,-1, 1, 1, 0,0,1,1,
-     1,-1,-1, 1, 0,1,1,1,
-     1, 1,-1, 1, 1,1,0,1,
-    -1, 1,-1, 1, 1,0,1,1,
-    -1,-1,-1, 1, 0,0,0,1,
-  ];
-
-  var DEFAULT_INDICES = new Uint8Array([
-    0, 1, 2,   0, 2, 3,    // front
-    0, 3, 4,   0, 4, 5,    // right
-    0, 5, 6,   0, 6, 1,    // up
-    1, 6, 7,   1, 7, 2,    // left
-    7, 4, 3,   7, 3, 2,    // down
-    4, 7, 6,   4, 6, 5     // back
- ]);
-
-  glUtils.SL.init({ callback:function() { main(); } });
-
-  function main() {
-    state.canvas = document.getElementById("glcanvas");
-    state.gl = glUtils.checkWebGL(state.canvas);
-    initCallbacks();
-    initShaders();
-    initGL();
-    animate();
+  function SignalBinding(signal, listener, isOnce, listenerContext, priority) {
+    this._listener = listener;
+    this._isOnce = isOnce;
+    this.context = listenerContext;
+    this._signal = signal;
+    this._priority = priority || 0;
   }
 
-  /*
-  * Initialization
-  * www.programmingtil.com
-  * www.codenameparkerllc.com
-  */
-  function initCallbacks() {
-    document.onkeydown = keydown;
-    document.onkeyup = keyup;
-    state.canvas.onmousedown = mousedown;
-    state.canvas.onmouseup = mouseup;
-    state.canvas.onmousemove = mousemove;
-  }
+  SignalBinding.prototype = {
+    active: true,
+    params: null,
+    execute: function(paramsArr) {
+      var handlerReturn, params;
+      if (this.active && !!this._listener) {
+        params = this.params ? this.params.concat(paramsArr) : paramsArr;
+        handlerReturn = this._listener.apply(this.context, params);
+        if (this._isOnce) {
+          this.detach();
+        }
+      }
+      return handlerReturn;
+    },
+    detach: function() {
+      return this.isBound() ? this._signal.remove(this._listener) : null;
+    },
+    isBound: function() {
+      return !!this._signal && !!this._listener;
+    },
+    getListener: function() {
+      return this._listener;
+    },
+    _destroy: function() {
+      delete this._signal;
+      delete this._listener;
+      delete this.context;
+    },
+    isOnce: function() {
+      return this._isOnce;
+    },
+    toString: function() {
+      return '[SignalBinding isOnce: ' + this._isOnce + ', isBound: ' + this.isBound() + ', active: ' + this.active + ']';
+    }
+  };
 
-  function initShaders() {
-    var vertexShader = glUtils.getShader(state.gl, state.gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex),
-      fragmentShader = glUtils.getShader(state.gl, state.gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
-    state.program = glUtils.createProgram(state.gl, vertexShader, fragmentShader);
-  }
-
-  function initGL() {
-    state.gl.clearColor(0,0,0,1);
-    state.gl.enable(state.gl.DEPTH_TEST);
-  }
-
-  /*
-  * Rendering / Drawing / Animation
-  * www.programmingtil.com
-  * www.codenameparkerllc.com
-  */
-  function animate() {
-    state.animation.tick = function() {
-      updateState();
-      draw();
-      requestAnimationFrame(state.animation.tick);
-    };
-    state.animation.tick();
-  }
-
-  function updateState() {
-    var speed = 0.2;
-    if (state.ui.pressedKeys[37]) {
-      // left
-      state.app.eye.x += speed;
-    } else if (state.ui.pressedKeys[39]) {
-      // right
-      state.app.eye.x -= speed;
-    } else if (state.ui.pressedKeys[40]) {
-      // down
-      state.app.eye.y += speed;
-    } else if (state.ui.pressedKeys[38]) {
-      // up
-      state.app.eye.y -= speed;
+  function validateListener(listener, fnName) {
+    if (typeof listener !== 'function') {
+      throw new Error('listener is a required param of {fn}() and should be a Function.'.replace('{fn}', fnName));
     }
   }
 
-  function draw(args) {
-    var v = (args && args.v) ? args.v : DEFAULT_VERT;
-    var vi = (args && args.vi) ? args.vi : DEFAULT_INDICES;
-    var uMVPMatrix = state.gl.getUniformLocation(state.program, 'uMVPMatrix');
-    var n = initVertexBuffers(v, vi).indices.length;
-    var mvm = mat4.create();
-    var pm = mat4.create();
-    var mvp = mat4.create();
+  signals.Signal = function() {
+    this._bindings = [];
+  };
 
-    mat4.perspective(pm,
-      20, 1/1, 1, 100
-    );
-    mat4.lookAt(mvm,
-      vec3.fromValues(state.app.eye.x,state.app.eye.y,state.app.eye.z),
-      vec3.fromValues(0,0,0),
-      vec3.fromValues(0,1,0)
-    );
-    mat4.copy(mvp, pm);
-    mat4.multiply(mvp, mvp, mvm);
-    mat4.rotateX(mvp, mvp, state.app.angle.x);
-    mat4.rotateY(mvp, mvp, state.app.angle.y);
-
-    state.gl.useProgram(state.program);
-    state.gl.clear(state.gl.COLOR_BUFFER_BIT | state.gl.DEPTH_BUFFER_BIT);
-    state.gl.uniformMatrix4fv(uMVPMatrix, false, mvp);
-    state.gl.drawElements(state.gl.TRIANGLES, n, state.gl.UNSIGNED_BYTE, 0);
-  }
-
-  function initVertexBuffers(v, i) {
-    var vertices = new Float32Array(v);
-    vertices.stride = 8;
-    vertices.attributes = [
-      {name:'aPosition', size:3, offset:0},
-      {name:'aColor',    size:3, offset:4},
-    ];
-    vertices.n = vertices.length/vertices.stride;
-    vertices.indices = i;
-    state.program.renderBuffers(vertices, i);
-    return vertices;
-  }
-
-  /*
-  * UI Events
-  * www.programmingtil.com
-  * www.codenameparkerllc.com
-  */
-  function keydown(event) {
-    state.ui.pressedKeys[event.keyCode] = true;
-  }
-
-  function keyup(event) {
-    state.ui.pressedKeys[event.keyCode] = false;
-  }
-
-  function mousedown(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var rect = event.target.getBoundingClientRect();
-    // If we're within the rectangle, mouse is down within canvas.
-    if (rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom) {
-      state.ui.mouse.lastX = x;
-      state.ui.mouse.lastY = y;
-      state.ui.dragging = true;
+  signals.Signal.prototype = {
+    _shouldPropagate: true,
+    active: true,
+    _registerListener: function(listener, isOnce, scope, priority) {
+      var prevIndex = this._indexOfListener(listener),
+        binding;
+      if (prevIndex !== -1) {
+        binding = this._bindings[prevIndex];
+        if (binding.isOnce() !== isOnce) {
+          throw new Error('You cannot add' + (isOnce ? '' : 'Once') + '() then add' + (!isOnce ? '' : 'Once') + '() the same listener without removing the relationship first.');
+        }
+      } else {
+        binding = new SignalBinding(this, listener, isOnce, scope, priority);
+        this._addBinding(binding);
+      }
+      return binding;
+    },
+    _addBinding: function(binding) {
+      var n = this._bindings.length;
+      do {
+        --n;
+      } while (this._bindings[n] && binding._priority <= this._bindings[n]._priority);
+      this._bindings.splice(n + 1, 0, binding);
+    },
+    _indexOfListener: function(listener) {
+      var n = this._bindings.length;
+      while (n--) {
+        if (this._bindings[n]._listener === listener) {
+          return n;
+        }
+      }
+      return -1;
+    },
+    add: function(listener, scope, priority) {
+      validateListener(listener, 'add');
+      return this._registerListener(listener, false, scope, priority);
+    },
+    addOnce: function(listener, scope, priority) {
+      validateListener(listener, 'addOnce');
+      return this._registerListener(listener, true, scope, priority);
+    },
+    remove: function(listener) {
+      validateListener(listener, 'remove');
+      var i = this._indexOfListener(listener);
+      if (i !== -1) {
+        this._bindings[i]._destroy();
+        this._bindings.splice(i, 1);
+      }
+      return listener;
+    },
+    removeAll: function() {
+      var n = this._bindings.length;
+      while (n--) {
+        this._bindings[n]._destroy();
+      }
+      this._bindings.length = 0;
+    },
+    getNumListeners: function() {
+      return this._bindings.length;
+    },
+    halt: function() {
+      this._shouldPropagate = false;
+    },
+    dispatch: function(params) {
+      if (!this.active) {
+        return;
+      }
+      var paramsArr = Array.prototype.slice.call(arguments),
+        bindings = this._bindings.slice(),
+        n = this._bindings.length;
+      this._shouldPropagate = true;
+      do {
+        n--;
+      } while (bindings[n] && this._shouldPropagate && bindings[n].execute(paramsArr) !== false);
+    },
+    dispose: function() {
+      this.removeAll();
+      delete this._bindings;
+    },
+    toString: function() {
+      return '[Signal active: ' + this.active + ' numListeners: ' + this.getNumListeners() + ']';
     }
-  }
+  };
 
-  function mouseup(event) {
-    state.ui.dragging = false;
-  }
-
-  function mousemove(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    if (state.ui.dragging) {
-      // The rotation speed factor
-      // dx and dy here are how for in the x or y direction the mouse moved
-      var factor = 10/state.canvas.height;
-      var dx = factor * (x - state.ui.mouse.lastX);
-      var dy = factor * (y - state.ui.mouse.lastY);
-
-      // update the latest angle
-      state.app.angle.x = state.app.angle.x + dy;
-      state.app.angle.y = state.app.angle.y + dx;
+  var uiUtils = {
+    VERSION: '0.0.2',
+    pixelInputToGLCoord: function(event, canvas) {
+      var x = event.clientX,
+        y = event.clientY,
+        midX = canvas.width / 2,
+        midY = canvas.height / 2,
+        rect = event.target.getBoundingClientRect();
+      x = ((x - rect.left) - midX) / midX;
+      y = (midY - (y - rect.top)) / midY;
+      return {
+        x: x,
+        y: y
+      };
+    },
+    pixelInputToCanvasCoord: function(event, canvas) {
+      var x = event.clientX,
+        y = event.clientY,
+        rect = event.target.getBoundingClientRect();
+      x = x - rect.left;
+      y = rect.bottom - y;
+      return {
+        x: x,
+        y: y
+      };
     }
-    // update the last mouse position
-    state.ui.mouse.lastX = x;
-    state.ui.mouse.lastY = y;
-  }
-})(window || this);
+  };
+
+  global.signals = signals;
+  global.uiUtils = uiUtils;
+
+}(window || this));
